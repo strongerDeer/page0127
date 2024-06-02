efa5840bc6526294009445a973f823477a15234f
strongerDeer <stronger_deer@naver.com>
2024-06-02 19:48:44 +0900

    📝 Edit: 폰트 설정 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,19 @@
 import dynamic from 'next/dynamic';
+import Banners from '@components/Banners';
 
 import { BookListSkeleton } from '@components/BookList';
-import Banners from '@components/Banners';
+
+export default function Home() {
+  return (
+    <main>
+
+
+      <BookList />
+    </main>
+  );
+}
 
 const BookList = dynamic(() => import('@components/BookList'), {
   ssr: false,
   loading: () => <BookListSkeleton />,
 });
-
-export default function Home() {
-  return (
-    <main>
-      <Banners />
-      <BookList />
-    </main>
-  );
-}
