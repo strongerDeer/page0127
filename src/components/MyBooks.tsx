b4ed8d0304e9ac379a4667da7ead44c76bc3d6d5
strongerDeer <stronger_deer@naver.com>
2024-08-27 23:08:45 +0900

    📝 Add: 최근 3년 탭 추가

--- src/components/MyBooks.tsx
+++ src/components/MyBooks.tsx
@@ -4,14 +4,24 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 import useMyBooks from '@hooks/useMyBooks';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import styles from './MyBooks.module.scss';
+import { Book } from '@models/book';
 
 export default function MyBooks({ pageUid }: { pageUid: string }) {
   const { data: book } = useMyBooks({ userId: pageUid });
 
   const [imgSrc, setImgSrc] = useState<{ [key: string]: string }>({});
+
+  const latestYear = new Date().getFullYear();
+
+  const latestBook = book?.filter(
+    (book) => book.readDate && book.readDate > `${latestYear}-01-01`,
+  ) as Book[];
+
+  const [activeYear, setActiveYear] = useState<number>(latestYear);
+  const [bookData, setBookData] = useState<Book[]>(latestBook);
 
   const onError = (bookId: string) => {
     setImgSrc((prev) => ({
@@ -19,26 +29,68 @@ export default function MyBooks({ pageUid }: { pageUid: string }) {
       [bookId]: '/images/no-book.jpg',
     }));
   };
+
+  useEffect(() => {
+    const filteredBook = book?.filter(
+      (book) =>
+        book.readDate &&
+        book.readDate > `${activeYear}-01-01` &&
+        book.readDate < `${activeYear + 1}-01-01`,
+    ) as Book[];
+
+    setBookData(filteredBook);
+  }, [activeYear, book]);
+
+  const handleYear = (e: React.MouseEvent<HTMLButtonElement>) => {
+    setActiveYear(Number(e.currentTarget.textContent));
+  };
   return (
-    <div className={styles.books}>
-      {book?.map((book) => (
-        <div className={book.grade === '10' ? styles.best : ''}>
-          <Link href={`/shelf/${pageUid}/${book.id}`} key={book.id}>
-            {book.id && (
-              <Image
-                src={
-                  imgSrc[book.id] ||
-                  (book.grade === '10' ? book.frontCover : book.flipCover)
-                }
-                alt=""
-                width={240}
-                height={240}
-                onError={() => onError(book.id as string)}
-              />
-            )}
-          </Link>
+    <div>
+      <ul className="flex gap-4">
+        <li>
+          <button type="button" onClick={handleYear}>
+            {latestYear}
+          </button>
+        </li>
+        <li>
+          <button type="button" onClick={handleYear}>
+            {latestYear - 1}
+          </button>
+        </li>
+        <li>
+          <button type="button" onClick={handleYear}>
+            {latestYear - 2}
+          </button>
+        </li>
+      </ul>
+      <h2>{activeYear} 읽은 도서</h2>
+      {bookData && bookData.length > 0 ? (
+        <div className={styles.books}>
+          {bookData?.map((book) => (
+            <div
+              className={book.grade === '10' ? styles.best : ''}
+              key={book.id}
+            >
+              <Link href={`/shelf/${pageUid}/${book.id}`}>
+                {book.id && (
+                  <Image
+                    src={
+                      imgSrc[book.id] ||
+                      (book.grade === '10' ? book.frontCover : book.flipCover)
+                    }
+                    alt=""
+                    width={240}
+                    height={240}
+                    onError={() => onError(book.id as string)}
+                  />
+                )}
+              </Link>
+            </div>
+          ))}
         </div>
-      ))}
+      ) : (
+        <div>데이터가 없습니다</div>
+      )}
     </div>
   );
 }
