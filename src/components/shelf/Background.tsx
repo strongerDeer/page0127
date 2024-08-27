ac58a5fc07cc5f8febf407c19b669fb6012d827e
strongerDeer <stronger_deer@naver.com>
2024-08-27 23:08:23 +0900

    📝 Edit: priority 추가

--- src/components/shelf/Background.tsx
+++ src/components/shelf/Background.tsx
@@ -4,7 +4,13 @@ import styles from './Background.module.scss';
 export default function Background() {
   return (
     <div className={styles.background}>
-      <Image src="/images/main-visual.jpg" alt="" width={1920} height={400} />
+      <Image
+        src="/images/main-visual.jpg"
+        alt=""
+        width={1920}
+        height={400}
+        priority
+      />
     </div>
   );
 }
