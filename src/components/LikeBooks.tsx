a1c40727feef5bfbcf946cb333f68cf40e8e5734
strongerDeer <stronger_deer@naver.com>
2024-10-03 08:28:13 +0900

    🔥 Remove: 불필요한 파일 삭제

--- src/components/LikeBooks.tsx
+++
@@ -1,7 +0,0 @@
-import useMyLikeBook from '@hooks/useMyLikeBook';
-
-export default function LikeBooks({ bookIds }: { bookIds: string[] }) {
-  let { data } = useMyLikeBook({ bookIds });
-
-  return <div>{data?.map((book) => <>{book.title}</>)}</div>;
-}
