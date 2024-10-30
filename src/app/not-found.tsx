01702d3ac0979b37abec3788bf29ec70c8ca55e5
strongerDeer <stronger_deer@naver.com>
2024-10-30 14:57:00 +0900

    ✨ Feat: not found 페이지

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,13 +2,30 @@
 import Button from '@components/shared/Button';
 import { useRouter } from 'next/navigation';
 
+import styles from './not-found.module.scss';
+import clsx from 'clsx';
+import { cormorant } from '@font';
+import Icon from '@components/icon/Icon';
 export default function NotFound() {
   const router = useRouter();
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Button onClick={() => router.back()}>이전페이지로 돌아가기</Button>
+    <div className={styles.notFound}>
+      <h2 className={cormorant.className}>
+        Not Found<span>.</span>
+      </h2>
+      <p>
+        페이지가 존재하지 않습니다.<span className="br"></span>입력하신 주소가
+        정확한지 다시 한번 확인해 주세요!
+      </p>
+      <div className={styles.btnGroup}>
+        <Button onClick={() => router.back()} size="lg" variant="outline">
+          이전 페이지
+        </Button>
+        <Button onClick={() => router.push('/')} size="lg">
+          <Icon name="home" color="#fff" />
+          메인 페이지
+        </Button>
+      </div>
     </div>
   );
 }
