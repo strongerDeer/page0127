3a4f107fad45731aa2a9012df90427ff51afa325
strongerDeer <stronger_deer@naver.com>
2024-11-18 23:01:08 +0900

    💄 Style: UI 수정

--- src/components/book/BookDetail.tsx
+++ src/components/book/BookDetail.tsx
@@ -75,92 +75,94 @@ export default function BookDetail({ data }: { data: Book }) {
   const avg = length > 0 ? (sum / length).toFixed(2) : '0.00';
 
   return (
-    <>
-      <div className={styles.bookDetail}>
-        <ScrollProgressBar />
-        <div className={styles.bg}>
-          <Image src={frontCover} width={100} height={100} alt="" priority />
-        </div>
-        <div className="max-width">
-          <div className={styles.top}>
-            <div className={styles.leftWrap}>
-              <motion.div
-                className={styles.bookImg}
-                initial={{ opacity: 0, translateY: '50%' }}
-                whileInView={{ opacity: 1, translateY: '0%' }}
-                transition={{ duration: 0.3 }}
-                viewport={{ once: true }}
-              >
-                <Image src={frontCover} width={320} height={320} alt="" />
-              </motion.div>
-              <div className={styles.buttons}>
-                {id && (
-                  <LikeButton
-                    bookId={id}
-                    isLike={likeBooks?.includes(id) || false}
-                    showText
-                  />
-                )}
-                {user && readUser?.includes(user.uid) ? (
-                  <p>읽었어요!</p>
-                ) : (
-                  <Link
-                    href={{ pathname: '/book/create', query: { bookId: id } }}
-                    className={styles.createMyBook}
-                  >
-                    읽은 책 등록
-                  </Link>
-                )}
-              </div>
-            </div>
-
-            <div className={styles.bookInfo}>
-              <h2>{title}</h2>
-              {subTitle && <h3>{subTitle}</h3>}
-              <p>{author}</p>
-
-              <p>
-                {publisher} {pubDate && format(pubDate, 'yyyy.MM.dd')}
-              </p>
-
-              <div className={styles.box}>
-                <div className={styles.inner}>
-                  <p>
-                    <span>분야</span>
-                    {category}
-                  </p>
-
-                  <p>
-                    <span>페이지</span>
-                    {page}
-                  </p>
-                  <p>
-                    <span>가격</span>
-                    {price}원
-                  </p>
-                </div>
-              </div>
-
-              <p className={styles.description}>{description}</p>
+    <div className={styles.bookDetail}>
+      <ScrollProgressBar />
+      <div className={styles.bg}>
+        <Image src={frontCover} width={100} height={100} alt="" priority />
+      </div>
+      <div className="max-width">
+        <div className={styles.top}>
+          <div className={styles.leftWrap}>
+            <motion.div
+              className={styles.bookImg}
+              initial={{ opacity: 0, translateY: '50%' }}
+              whileInView={{ opacity: 1, translateY: '0%' }}
+              transition={{ duration: 0.3 }}
+              viewport={{ once: true }}
+            >
+              <Image src={frontCover} width={320} height={320} alt="" />
+            </motion.div>
+            <div className={styles.buttons}>
+              {id && (
+                <LikeButton
+                  bookId={id}
+                  isLike={likeBooks?.includes(id) || false}
+                  showText
+                />
+              )}
+              {user && readUser?.includes(user.uid) ? (
+                <p>읽었어요!</p>
+              ) : (
+                <Link
+                  href={{ pathname: '/book/create', query: { bookId: id } }}
+                  className={styles.createMyBook}
+                >
+                  읽은 책 등록
+                </Link>
+              )}
             </div>
           </div>
 
-          {user && readUser?.includes(user.uid) && (
-            <section className={clsx('section', styles.myContents)}>
-              <h3 className="sub_title">나의 메모</h3>
-              <div className={styles.myWrap}>
+          <div className={styles.bookInfo}>
+            <h2>{title}</h2>
+            {subTitle && <h3>{subTitle}</h3>}
+            <p>{author}</p>
+
+            <p>
+              {publisher} {pubDate && format(pubDate, 'yyyy.MM.dd')}
+            </p>
+
+            <div className={styles.box}>
+              <div className={styles.inner}>
                 <p>
-                  {Number(myBook?.grade) !== 10 ? (
-                    <>
-                      {'⭐️'.repeat(Number(myBook?.grade))} {myBook?.grade}점
-                    </>
-                  ) : (
-                    <>나의 인생책!</>
-                  )}
+                  <span>분야</span>
+                  {category}
                 </p>
-                <p>완독일: {myBook?.readDate}</p>
+
+                <p>
+                  <span>페이지</span>
+                  {page}
+                </p>
+                <p>
+                  <span>가격</span>
+                  {price}원
+                </p>
               </div>
-              {myBook?.memo && <p className={styles.memo}>{myBook?.memo}</p>}
+            </div>
+
+            <p className={styles.description}>{description}</p>
+          </div>
+        </div>
+
+        {user && readUser?.includes(user.uid) && (
+          <section className={clsx('section', styles.myContents)}>
+            <div className={styles.memoTop}>
+              <h3>나의 메모</h3>
+
+              <p>
+                {Number(myBook?.grade) !== 10 ? (
+                  <>
+                    {'⭐️'.repeat(Number(myBook?.grade))} {myBook?.grade}점
+                  </>
+                ) : (
+                  <>나의 인생책!</>
+                )}
+              </p>
+              <p className={styles.readDate}>완독일: {myBook?.readDate}</p>
+            </div>
+
+            {myBook?.memo && <p className={styles.memo}>{myBook?.memo}</p>}
+            <div className={styles.btns}>
               <Button href={`/my/${id}/edit`}>수정하기</Button>
               <Button
                 variant="outline"
@@ -191,31 +193,31 @@ export default function BookDetail({ data }: { data: Book }) {
                 <Icon name="delete" color="error" />
                 삭제
               </Button>
-            </section>
-          )}
+            </div>
+          </section>
+        )}
 
+        <section className="section">
+          <h3 className="sub_title">
+            이 책을 읽은 리더들 <span>{readUser?.length}</span>
+          </h3>
+
+          <p>평점: ⭐️⭐️⭐️⭐️⭐️{avg}</p>
+        </section>
+
+        {grade && grade[10] && (
           <section className="section">
             <h3 className="sub_title">
-              이 책을 읽은 리더들 <span>{readUser?.length}</span>
+              이 책을 읽은 인생책으로 뽑은 리더들{' '}
+              <span>{getGradeLength(grade, '10')}</span>
             </h3>
-
-            <p>평점: ⭐️⭐️⭐️⭐️⭐️{avg}</p>
+            <LifeUsers userIds={grade[10] as string[]} />
           </section>
+        )}
 
-          {grade && grade[10] && (
-            <section className="section">
-              <h3 className="sub_title">
-                이 책을 읽은 인생책으로 뽑은 리더들{' '}
-                <span>{getGradeLength(grade, '10')}</span>
-              </h3>
-              <LifeUsers userIds={grade[10] as string[]} />
-            </section>
-          )}
-
-          {id && <Review bookId={id} />}
-        </div>
+        {id && <Review bookId={id} />}
       </div>
-    </>
+    </div>
   );
 }
 
