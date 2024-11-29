ac476beef38d6272b3f0781b8b8459ba623f9967
strongerDeer <stronger_deer@naver.com>
2024-11-29 21:12:57 +0900

    ♻️ Refactor: 에러 추가

--- src/app/(shelf)/[userId]/page.tsx
+++ src/app/(shelf)/[userId]/page.tsx
@@ -1,4 +1,5 @@
 import ShelfPage from '@components/templates/ShelfPage';
+import { User } from '@connect/user';
 import { COLLECTIONS } from '@constants';
 import { store } from '@firebase/firebaseApp';
 import { doc, getDoc } from 'firebase/firestore';
@@ -10,9 +11,16 @@ export const revalidate = 60;
 export default async function page({ params }: { params: { userId: string } }) {
   const { userId } = params;
   const userDoc = await getDoc(doc(store, COLLECTIONS.USER, userId));
+
   if (!userDoc.exists()) {
     notFound();
   }
 
-  return <ShelfPage userId={userId} />;
+  const userData = userDoc.data() as User;
+
+  if (!userData) {
+    throw new Error('유저 데이터 없음.');
+  }
+
+  return <ShelfPage userData={userData} />;
 }
