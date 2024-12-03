78c19a79fc077b75c08e289d459b76c2040ef6f5
strongerDeer <stronger_deer@naver.com>
2024-12-03 23:33:00 +0900

    ✨ Feat: 무한 스크롤, 정렬 기능 추가

--- src/app/book/page.tsx
+++ src/app/book/page.tsx
@@ -1,9 +1,56 @@
 'use client';
+import Loading from '@components/Loading';
 import BookList from '@components/book/BookList';
 import useBooks from '@hooks/useBooks';
+import { useInView } from 'framer-motion';
+import { useEffect, useRef, useState } from 'react';
 
 export default function Home() {
-  const { data } = useBooks();
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    sortBy,
+    setSortBy,
+  } = useBooks();
 
-  return <main>{data && <BookList data={data} />}</main>;
+  const ref = useRef(null);
+  const isInView = useInView(ref);
+  const [isLoadingMore, setIsLoadingMore] = useState(false);
+
+  useEffect(() => {
+    if (isInView && hasNextPage && !isFetchingNextPage && !isLoadingMore) {
+      setIsLoadingMore(true);
+      // 1초 후에 다음 페이지 로드
+      setTimeout(() => {
+        fetchNextPage();
+        setIsLoadingMore(false);
+      }, 1000);
+    }
+  }, [isInView, hasNextPage, isFetchingNextPage, isLoadingMore, fetchNextPage]);
+  return (
+    <main className="max-width">
+      <div className="flex items-center justify-between">
+        <h2 className="title2">도서</h2>
+        <select
+          value={sortBy}
+          onChange={(e) => setSortBy(e.target.value as any)}
+          className="w-[180px] p-2 border rounded-md"
+        >
+          <option value="인기순">인기순</option>
+          <option value="인생책순">인생책순</option>
+          <option value="등록순">등록순</option>
+          <option value="이름순">이름순</option>
+          <option value="출시일순">출시일순</option>
+        </select>
+      </div>
+
+      {data && <BookList data={data} />}
+
+      <div ref={ref} className="h-60 flex items-center justify-center">
+        {(isFetchingNextPage || isLoadingMore) && <Loading />}
+      </div>
+    </main>
+  );
 }
--- src/hooks/useBooks.ts
+++ src/hooks/useBooks.ts
@@ -1,33 +1,129 @@
 'use client';
-import { useQuery, useQueryClient } from 'react-query';
+import { useInfiniteQuery, useQuery, useQueryClient } from 'react-query';
 
-import { useEffect } from 'react';
-import { collection, onSnapshot, orderBy, query } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
+import {
+  collection,
+  getDoc,
+  getDocs,
+  limit,
+  onSnapshot,
+  orderBy,
+  query,
+  startAfter,
+} from 'firebase/firestore';
 import { COLLECTIONS } from '@constants';
 import { store } from '@firebase/firebaseApp';
 import { getBooks } from '@connect/book/books';
+import { Book } from '@connect/book';
 
-export default function useBooks() {
+type SortOption = '인생책순' | '인기순' | '등록순' | '이름순' | '출시일순';
+
+export default function useBooks(initialSort: SortOption = '이름순') {
+  const [sortBy, setSortBy] = useState<SortOption>(initialSort);
   const client = useQueryClient();
+
+  const getSortField = (sort: SortOption) => {
+    switch (sort) {
+      case '인생책순':
+        return 'grade10Count';
+      case '인기순':
+        return 'readUserCount';
+      case '등록순':
+        return 'lastUpdatedTime';
+      case '이름순':
+        return 'title';
+      case '출시일순':
+        return 'pubDate';
+      default:
+        return 'createdTime';
+    }
+  };
+
+  const getSortDirection = (sort: SortOption) => {
+    if (['등록순', '인생책순', '인기순'].includes(sort)) {
+      return 'desc';
+    }
+    return 'asc';
+  };
 
   useEffect(() => {
     const unsubscribe = onSnapshot(
       query(
         collection(store, COLLECTIONS.BOOKS),
-        orderBy('createdTime', 'desc'),
+        orderBy(getSortField(sortBy), getSortDirection(sortBy)),
+        limit(12),
       ),
       (snapshot) => {
         const newBooks = snapshot.docs.map((doc) => ({
           id: doc.id,
           ...doc.data(),
         }));
-        client.setQueryData(['books'], newBooks);
+
+        //첫 페이지 데이터만 업데이트
+        client.setQueryData(['books', sortBy], (old: any) => ({
+          pages: [
+            {
+              items: newBooks,
+              lastVisible: snapshot.docs[snapshot.docs.length - 1],
+            },
+          ],
+          pageParams: [null],
+        }));
       },
     );
     return () => {
       unsubscribe();
     };
-  }, [client]);
+  }, [client, sortBy]);
 
-  return useQuery(['books'], () => getBooks(), { suspense: true });
+  // 무한 스크롤
+  const fetchBooks = async ({ pageParam = null }) => {
+    let bookQuery = query(
+      collection(store, COLLECTIONS.BOOKS),
+      orderBy(getSortField(sortBy), getSortDirection(sortBy)),
+      limit(12),
+    );
+
+    if (pageParam) {
+      bookQuery = query(
+        collection(store, COLLECTIONS.BOOKS),
+        orderBy(getSortField(sortBy), getSortDirection(sortBy)),
+        startAfter(pageParam),
+        limit(12),
+      );
+    }
+
+    const snapshot = await getDocs(bookQuery);
+    const lastVisible = snapshot.docs[snapshot.docs.length - 1];
+
+    return {
+      items: snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...(doc.data() as Book),
+      })),
+      lastVisible,
+    };
+  };
+
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage } =
+    useInfiniteQuery(
+      ['books', sortBy],
+      ({ pageParam }) => fetchBooks({ pageParam }),
+      {
+        getNextPageParam: (lastPage) =>
+          lastPage.items.length === 12 ? lastPage.lastVisible : undefined,
+        suspense: true,
+      },
+    );
+
+  return {
+    data: data?.pages.flatMap((page) => page.items) ?? [],
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    sortBy,
+    setSortBy,
+  };
+  // return useQuery(['books'], () => getBooks(), { suspense: true });
 }
