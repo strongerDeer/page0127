82215edfa774d9511788f61959a85578afecf6b3
strongerDeer <stronger_deer@naver.com>
2024-12-25 14:16:15 +0900

    🧪 Test: 배포 테스트

---
+++ src/app/test/page.tsx
@@ -0,0 +1,17 @@
+import BarChart from '@components/my/BarChart';
+import RadarChart from '@components/my/RadarChart';
+import useUserCount from '@connect/user/useUserCount';
+
+export default function Page() {
+  const { data: counterData } = useUserCount('dreamfulbud', '2024');
+  return (
+    <div>
+      {counterData && (
+        <>
+          {/* <BarChart title="ddd" userData={counterData.date} year="2024" /> */}
+          {/* <RadarChart title={'ddd'} userData={counterData.category || {}} /> */}
+        </>
+      )}
+    </div>
+  );
+}
