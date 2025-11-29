3425fb8fcd0b36b1ba6517866535a5804ece097d
strongerDeer <stronger_deer@naver.com>
2025-11-29 17:19:50 +0900

    🚚 Move: FSD에 맞게 파일 이동 및 설정 수정

--- apps/page0127/middleware.ts
+++ apps/page0127/middleware.ts
@@ -1,5 +1,5 @@
 import { type NextRequest } from 'next/server'
-import { updateSession } from '@/lib/supabase/middleware'
+import { updateSession } from '@/shared/config/supabase/middleware'
 
 /**
  * Next.js Middleware
